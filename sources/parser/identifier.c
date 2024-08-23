8074e0077c259dbea23d60174d859cfcb61a28e3
atormac <atorma@proton.me>
2024-08-23 19:12:36 +0300

    Massive simplification

--- sources/parser/identifier.c
+++ sources/parser/identifier.c
@@ -28,27 +28,6 @@ static int	validate_object_count(char **line, int id)
 	return (1);
 }
 
-static int	check_duplicates(char ***elements, int i)
-{
-	int		j;
-	size_t	len;
-
-	j = 0;
-	while (j < i)
-	{
-		len = ft_strlen(elements[i][0]);
-		if (len != 1 || (*elements[i][0] < 'A' && *elements[i][0] > 'Z'))
-			break ;
-		if (ft_strcmp(elements[j][0], elements[i][0]) == 0)
-		{
-			printf("duplicate found: %s\n", elements[j][0]);
-			return (0);
-		}
-		j++;
-	}
-	return (1);
-}
-
 static int	identifiers_check(char ***elements)
 {
 	size_t	i;
@@ -62,13 +41,10 @@ static int	identifiers_check(char ***elements)
 		id = identifier_type(elements[i][0]);
 		if (id == ID_UNKNOWN)
 			return (0);
-		if (id == ID_CAMERA)
-			found[0] += 1;
-		if (id == ID_AMBIENT)
-			found[1] += 1;
+		found[id] += 1;
 		i++;
 	}
-	if (!found[0] || !found[1])
+	if (found[ID_CAMERA] != 1 || found[ID_AMBIENT] != 1 || found[ID_LIGHT] != 1)
 		return (0);
 	return (1);
 }
@@ -84,12 +60,7 @@ int	identifiers_validate(char ***elements)
 	{
 		if (!validate_object_count(elements[i], identifier_type(elements[i][0])))
 		{
-			printf("Invalid object count!\n");
-			return (0);
-		}
-		if (!check_duplicates(elements, i))
-		{
-			printf("Duplicate entries found!\n");
+			printf("Error: Invalid object count!\n");
 			return (0);
 		}
 		i++;
