1eeff35bf6df0ebd186a241c0d16ca280078f3c3
atormac <atorma@proton.me>
2024-09-02 22:40:27 +0300

    cleanup parser

--- includes/parser.h
+++ includes/parser.h
@@ -13,22 +13,21 @@ enum
 	ID_UNKNOWN
 };
 
-void	parser_error(char *err_str);
-int		str_isdigit(char *s);
-int		str_isdouble(char *s);
+int	parser_error(char *err_str);
+int	str_isdigit(char *s);
+int	str_isdouble(char *s);
 size_t	str_charcount(char *s, int c);
 size_t	len_till(char *s, int c);
-
 size_t	array_size(char **arr);
 void	array_free(char **arr);
 double	ft_atof(const char *s);
-int		fill_vector(t_vct *vector, char	*s);
-int		fill_color(t_clr *vector, char	*s);
-int		identifier_type(char *id);
-int		parse_file(char *file, t_scene *scene);
-int		validate_ratio(char	*s, double min, double max);
-int		validate_vector(char *s);
-int		light_add_ambient(t_ambient *ambient, char **elem);
-int		light_add_point(t_light *light, char **elem);
+int	fill_vector(t_vct *vector, char	*s);
+int	fill_color(t_clr *vector, char	*s);
+int	identifier_type(char *id);
+int	parse_file(char *file, t_scene *scene);
+int	validate_ratio(char	*s, double min, double max);
+int	validate_vector(char *s);
+int	light_add_ambient(t_ambient *ambient, char **elem);
+int	light_add_point(t_light *light, char **elem);
 
 #endif
--- sources/parser/parser.c
+++ sources/parser/parser.c
@@ -14,21 +14,18 @@
 #include "../../includes/miniRT.h"
 #include "../../includes/parser.h"
 
-size_t	array_size(char **arr);
-void	array_free(char **arr);
 char	***array_matrix(char *data);
 void	array_matrix_free(char ***arr);
 char	*file_load(char *file);
 int	identifiers_validate(char ***elements);
-t_vct	ft_create_vct(double x, double y, double z);
-t_clr	ft_create_clr(double r, double g, double b);
 int	cam_add(t_scene *scene, char **elem);
 int	light_add(t_scene *scene, char **elem, int id);
 int	obj_add(t_scene *scene, char **elem, int id);
 
-void	parser_error(char *err_str)
+int	parser_error(char *err_str)
 {
 	printf("Error\n%s\n", err_str);
+	return (0);
 }
 
 int	parse_object(t_scene *scene, char **line)
@@ -53,19 +50,13 @@ int	parse_scene(t_scene *scene, char ***matrix)
 
 	i = 0;
 	if (!scene_init(scene))
-	{
-		parser_error("failed to initialize scene");
-		return (0);
-	}
+		return (parser_error("failed to initialize scene"));
 	if (!identifiers_validate(matrix))
 		return (0);
 	while (matrix[i])
 	{
 		if (!parse_object(scene, matrix[i]))
-		{
-			parser_error("malfored object information detected");
-			return (0);
-		}
+			return (parser_error("malfored object information detected"));
 		i++;
 	}
 	return (1);
@@ -85,9 +76,8 @@ int	parse_file(char *file, t_scene *scene)
 	matrix = array_matrix(data);
 	if (!matrix)
 	{
-		parser_error("ft_split failed");
 		free(data);
-		return (0);
+		return (parser_error("ft_split failed"));
 	}
 	ret = parse_scene(scene, matrix);
 	array_matrix_free(matrix);
