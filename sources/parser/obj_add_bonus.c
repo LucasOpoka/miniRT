5629725bc6c8bd9aa6ae38ae462cbf404cdb5d8f
atormac <atorma@student.hive.fi>
2024-09-14 19:27:49 +0300

    bonus parse cone from file

--- sources/parser/obj_add_bonus.c
+++ sources/parser/obj_add_bonus.c
@@ -6,7 +6,7 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 18:07:54 by atorma            #+#    #+#             */
-/*   Updated: 2024/09/14 19:10:24 by atorma           ###   ########.fr       */
+/*   Updated: 2024/09/14 19:26:55 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,22 @@
 
 int			validate_orientation(t_vct v);
 int			obj_add_bonus_fields(t_obj *obj, char **elem);
+
+int	cone_add(t_obj *obj, char **elem)
+{
+	obj->type = t_cone;
+	fill_vector(&obj->pos, elem[1]);
+	fill_vector(&obj->orientation, elem[2]);
+	obj->radius = ft_atof(elem[3]);
+	obj->height = ft_atof(elem[4]);
+	if (obj->radius <= 0.0 || obj->height <= 0.0)
+		return (0);
+	if (!fill_color(&obj->color, elem[5]))
+		return (0);
+	if (!validate_orientation(obj->orientation))
+		return (0);
+	return (1);
+}
 
 int	obj_add(t_scene *scene, char **elem, int id)
 {
@@ -35,6 +51,8 @@ int	obj_add(t_scene *scene, char **elem, int id)
 		ret = plane_add(obj, elem);
 	else if (id == e_id_cylinder)
 		ret = cylinder_add(obj, elem);
+	else if (id == e_id_cone)
+		ret = cone_add(obj, elem);
 	if (ret)
 		ret = obj_add_bonus_fields(obj, elem);
 	return (ret);
