41cbb6984f29417c325b50b4c398be66b7430879
atormac <atorma@student.hive.fi>
2024-09-20 16:49:50 +0300

    remove BVH define

--- includes/bvh.h
+++ includes/bvh.h
@@ -6,7 +6,7 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 16:52:41 by atorma            #+#    #+#             */
-/*   Updated: 2024/09/17 20:23:31 by atorma           ###   ########.fr       */
+/*   Updated: 2024/09/20 16:46:19 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,10 @@
 
 #include "miniRT.h"
 
-# define BVH 1
 # define BVH_MAX_NODES 10 * 1024
 # define BVH_MIN_CHILDREN 1
 
 t_node	*bvh_build(t_scene *scene);
 void	bvh_intersect(t_ray ray, t_scene* scene, t_xs *xs);
-//void	bvh_intersect(t_ray ray, t_scene *scene, uint32_t index, t_xs *xs);
+
 #endif
